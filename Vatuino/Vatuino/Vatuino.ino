5a7572fdac381ef5be859c0ab7be57221be8657a
Júlio Peres <jcrp.peres@gmail.com>
2020-06-12 12:34:01 -0300

    First vat

---
+++ Vatuino/Vatuino/Vatuino.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		Vatuino.ino
+ Created:	12/06/2020 12:28:54
+ Author:	Júlio Peres
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
